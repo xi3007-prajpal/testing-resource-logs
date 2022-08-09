bce574ae6292af5c6ab42647fae96a42796e8bf4
xlr8s <xlr8s@xebia.com>
2022-08-09 18:57:44 +0000

    added terraform resources

---
+++ _terraform_modules/xebia/terraform-aws-xebia-eks@v1.4.0/outputs.tf
@@ -0,0 +1,29 @@
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane."
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane."
+  value       = module.eks.cluster_security_group_id
+}
+
+# output "aws_auth_configmap_yaml" {
+#   description = "A kubernetes configuration to authenticate to this EKS cluster."
+#   value       = module.eks.aws_auth_configmap_yaml
+# }
+
+output "region" {
+  description = "AWS region."
+  value       = var.region
+}
+
+output "node_groups" {
+  description = "Outputs from node groups"
+  value       = module.eks.eks_managed_node_groups
+}
+
+output "cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster OIDC Issuer"
+  value       = module.eks.cluster_oidc_issuer_url
+}
